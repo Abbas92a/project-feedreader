eebffb5cc46eed0f80e551aa14e1cbe41625480a
Mohamed Abbas <mohamedabbas92a@gmail.com>
2018-06-26 22:10:58 +0200

    Test: content should change when a new feed is loaded

--- jasmine/spec/feedreader.js
+++ jasmine/spec/feedreader.js
@@ -102,15 +102,29 @@ $(function() {
     });
 
 		/* TODO: Write a new test suite named "New Feed Selection" */
-	 // describe('The menu', function() {
-		//  /* TODO: Write a test that ensures when a new feed is loaded
-		//  * by the loadFeed function that the content actually changes.
-		//  * Remember, loadFeed() is asynchronous.
-		//  */
-		//  it('change visibility in respose to clicking the icon', function() {
-	 //
-		//  });
-	 // });
+	 describe('New Feed Selection', function() {
+		 /* TODO: Write a test that ensures when a new feed is loaded
+		 * by the loadFeed function that the content actually changes.
+		 * Remember, loadFeed() is asynchronous.
+		 */
+		 var oldFeed, newFeed;
+		beforeEach(function(done) {
+			 $('.feed').empty();
+ 			loadFeed(0, function() {
+				oldFeed = $('.feed').html();
 
+				loadFeed(1, function() {
+					newFeed =  $('.feed').html();
+					done();
+				});
+ 			});
+
+ 		});
+
+ 		it('content should change when a new feed is loaded', function(done) {
+ 			expect(oldFeed).not.toEqual(newFeed);
+ 			done();
+ 		});
+	 });
 
 }());
